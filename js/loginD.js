9aa78bba9223d124fff9a1cc58ffc0210e5a67d5
JacobDolly <AnthonyLuiggyDollyO@gmail.com>
2020-03-23 22:23:28 -0500

    proyecto avance 2 del 2019

---
+++ js/loginD.js
@@ -0,0 +1,11 @@
+function validar(){
+	var user;
+	var password;
+	user=frmlogin.txtuser.value;
+	password=frmlogin.txtpassword.value;
+	if(user=="luis" &&password="1234")
+alert("Bienvenido usuario Luis");
+frmlogin:submit();
+else 
+	alert("Error");
+}
